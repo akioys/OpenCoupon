43bb1789044dc51a017e276e8902701351955439
akioys <w-bear@wb3.so-net.ne.jp>
2013-07-22 02:15:12 +0900

    correct E_NOTICE error in coupon_top.phtml

--- coupon/index.php
+++ coupon/index.php
@@ -9,14 +9,22 @@ $this->mark("coupon_id=$coupon_id",'debug');
 
 //  クーポンのrecord
 $select = $this->config()->select_coupon_list($coupon_id);
-$record = $this->pdo()->select($select);
+//$record = $this->pdo()->select($select);
+//$this->d($coupon_id);
+$record = $this->GetTCoupon($coupon_id);
+
+//	ショップのデータを取得
+//$this->d($record);
+$t_shop = $this->GetTShop($record['shop_id']);
+//$this->d($t_shop);
 
 //  templateに渡すdata
 $data = new Config();
 
 switch( $action ){
 	case 'index':
-		$this->template("index.phtml",array('coupon_list'=>$record));
+		//$this->template("index.phtml",array('coupon_list'=>$record, 't_shop'=>$t_shop));
+		$this->template("index.phtml",array('t_coupon'=>$record, 't_shop'=>$t_shop));
 		break;
 		
 	default:
